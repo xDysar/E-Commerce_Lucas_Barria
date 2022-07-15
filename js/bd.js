6e1c289644fa783d45490c45b60ffe72a23d14af
xDysar <lucasbnicolas@gmail.com>
2022-07-15 11:22:24 -0300

    V2 - Notificaciones carrito

---
+++ js/bd.js
@@ -0,0 +1,81 @@
+// Arreglo de objetos con hoodies oversize
+const hoodiesItems = [
+  {
+    id: 1,
+    name: "Geisha",
+    price: 10300.0,
+    color: "negro",
+    img: "../img/back-negro-over.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Bad Influence",
+    price: 9200.0,
+    color: "blanco",
+    img: "../img/bad-influence-oversize.jpg",
+  },
+  {
+    id: 3,
+    name: "Flame",
+    price: 9200.0,
+    color: "amarillo",
+    img: "../img/flame-amariillo-over.jpg",
+  },
+  {
+    id: 4,
+    name: "glory",
+    price: 10000.0,
+    color: "rosa",
+    img: "../img/glory-back-rosa-over.jpg",
+  },
+  {
+    id: 5,
+    name: "ipsum",
+    price: 12500.0,
+    color: "negro",
+    img: "../img/black-back11.jpg",
+  },
+  {
+    id: 6,
+    name: "lorem",
+    price: 7900.0,
+    color: "marron",
+    img: "../img/hoodie-back-marron-ove.jpg",
+  },
+  {
+    id: 7,
+    name: "lorem",
+    price: 8900.0,
+    color: "amarillo",
+    img: "../img/hoodie-amarilo-back-over.jpg",
+  },
+  {
+    id: 8,
+    name: "lorem",
+    price: 9500.0,
+    color: "blanco",
+    img: "../img/blanco-cyber-espalda.jpg",
+  },
+  {
+    id: 9,
+    name: "lorem",
+    price: 9000.0,
+    color: "blanco",
+    img: "../img/flame-banana-back-over.jpg",
+  },
+  {
+    id:  10,
+    name: "lorem",
+    price: 9100.0,
+    color: "rosa",
+    img: "../img/sakura-back-pver.jpg",
+  },
+  {
+    id:  11,
+    name: "lorem",
+    price: 9300.0,
+    color: "negro",
+    img: "../img/over-black-back.jpg",
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,30 +1,50 @@
-// Variables
+// Variables carrito
 const cart = document.querySelector("#cart");
 const containerCart = document.querySelector("#list-cart tbody");
 const emptyCart = document.querySelector("#empty-cart");
 const listHoodies = document.querySelector("#list-of-hoodies");
 let itemsCart = [];
 
+// Variables Hoodies
+const hoodieContainer = document.querySelector("#hoodieContainer");  // Contenedor hoodies
+
 loadEventListeners();
 function loadEventListeners() {
+  // Hoodies
+  document.addEventListener("DOMContentLoaded", () => {
+    hoodieHTML(hoodiesItems); // Muestra los hoodies
+  });
+
   // Cuando se agrega un hoodie presionando "agregar al carrito"
-  listHoodies.addEventListener('click', addProduct);
+  listHoodies.addEventListener("click", addProduct);
 
   // Elimina productos del carrito
-  cart.addEventListener('click', deleteProduct);
+  cart.addEventListener("click", deleteProduct);
 
   // Muestra los cursos del Local Storage
-  document.addEventListener('DOMContentLoaded', ()=>{
-    itemsCart = JSON.parse( localStorage.getItem('cart')) || [];
-
+  document.addEventListener("DOMContentLoaded", () => {
+    itemsCart = JSON.parse(localStorage.getItem("cart")) || [];
     cartHTML();
   });
 
   // Vaciar el carrito
-  emptyCart.addEventListener('click', ()=>{
+  emptyCart.addEventListener("click", () => {
     itemsCart = []; // Reseteamos el arreglo
-    cleanHTML()  // Eliminamos todo el HTML
-  })
+    cleanHTML(); // Eliminamos todo el HTML
+    Toastify({
+      // Notificacion de vaciado de carrito
+      text: "Limpiaste el carrito",
+      duration: 1500,
+      className: "notificationCart",
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "red",
+      },
+    }).showToast();
+  });
+
 }
 
 // Funciones
@@ -33,6 +53,19 @@ function addProduct(e) {
   if (e.target.classList.contains("btn-add-cart")) {
     const selectedProduct = e.target.parentElement;
     readDataProduct(selectedProduct);
+
+    Toastify({
+      // Notificacion de agregar un producto
+      text: "Se agrego al carrito",
+      duration: 1500,
+      className: "notificationCart",
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "#008542",
+      },
+    }).showToast();
   }
 }
 
@@ -44,6 +77,18 @@ function deleteProduct(e) {
     itemsCart = itemsCart.filter((product) => product.id !== productId);
 
     cartHTML(); // Iterar sobre el carrito y mostrar su HTML
+    Toastify({
+      // Notificacion de agregar un producto
+      text: "Eliminaste un producto del carrito",
+      duration: 1500,
+      className: "notificationCart",
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "#303030",
+      },
+    }).showToast();
   }
 }
 // Lee el HTML al que le dimos click y extrae la informacion del producto
@@ -56,21 +101,21 @@ function readDataProduct(product) {
     name: product.querySelector("h5").textContent,
     price: product.querySelector(".hoodie__price").textContent,
     id: product.querySelector(".btn-add-cart").getAttribute("data-id"),
-    amount: 1
+    amount: 1,
   };
 
-  // Revisa si un elemento ya existe en el carrito 
-  const existe = itemsCart.some( product => product.id === infoProduct.id);
+  // Revisa si un elemento ya existe en el carrito
+  const existe = itemsCart.some((product) => product.id === infoProduct.id);
   if (existe) {
-    const products = itemsCart.map(product =>{
+    const products = itemsCart.map((product) => {
       if (product.id === infoProduct.id) {
         product.amount++;
-        return product;  // Retorna el objeto actualizado
+        return product; // Retorna el objeto actualizado
       } else {
-        return product;  // Retorna los objetos que no son los duplicados
+        return product; // Retorna los objetos que no son los duplicados
       }
     });
-    itemsCart = [...products]
+    itemsCart = [...products];
   } else {
     // Agrega elementos al arreglo del carrito
     itemsCart = [...itemsCart, infoProduct];
@@ -86,9 +131,9 @@ function cartHTML() {
   cleanHTML(); // Limpiar el HTML
 
   // Recorre el carrito y genera el HTML
-  itemsCart.forEach( product => {
+  itemsCart.forEach((product) => {
     const { img, name, price, amount, id } = product;
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
     row.innerHTML = `
         <td>
           <img src="${img}">
@@ -110,7 +155,7 @@ function cartHTML() {
 }
 
 function syncStorage() {
-  localStorage.setItem('cart', JSON.stringify(itemsCart));
+  localStorage.setItem("cart", JSON.stringify(itemsCart));
 }
 
 // Limpiar el HTML previo
@@ -118,4 +163,29 @@ function cleanHTML() {
   while (containerCart.firstChild) {
     containerCart.removeChild(containerCart.firstChild);
   }
+}
+// Crear el HTML de los productos
+function hoodieHTML(hoodiesItems) {
+  hoodiesItems.forEach((hoodie) => {
+    const hoodieHTML = document.createElement("div");
+    hoodieHTML.classList.add('product-hoodie')
+
+    hoodieHTML.innerHTML = `
+          <a href="#">
+            <img class="hoodie__img" src="${hoodie.img}" alt="Hoodie">
+          </a>
+          <h5 class="hoodie__name">${hoodie.name.toUpperCase()}</h5>
+          <p class="hoodie__price">${hoodie.price}</p>
+          <div class="hoodie__dues">
+              <p class="hoodie__dues-info">6 cuotas sin interés de</p>
+              <p class="hoodie__dues-price">${Math.round(hoodie.price / 6)}</p>
+          </div>
+          <a class="btn-add-cart" href="#" id="hoodieBtn" data-id="${
+            hoodie.id
+          }">Agregar al carrito</a>
+    `;
+
+    // Imprimir en el HTML
+    hoodieContainer.appendChild(hoodieHTML);
+  });
 }
